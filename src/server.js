7f5112d852a0780ae46468633b5edec0756feda9
jeshua <ensongjeshua@gmail.com>
2017-08-10 06:39:24 +0800

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const Hapi = require( 'hapi' );
+const Path = require( 'path' );
+
+const server = new Hapi.Server();
+
+server.connection( {
+	port : 8080,
+	host : 'localhost'
+} )
+
+server.register( require( 'inert' ), function ( err ) {
+	if ( err ) {
+		throw err;
+	}
+
+	server.route( {
+		method : 'GET',
+		path : '/{param*}',
+		handler : {
+			directory :{
+				path : process.env.DOCUMENT_DIR,
+				listing : true
+			}
+		}
+	} )
+
+} );
+
+server.start( function ( err ) {
+	if ( err ) {
+		return console.log( 'Failed to start server' + err );
+	}
+
+	console.log( 'Server started:' + server.info.uri );
+} )
